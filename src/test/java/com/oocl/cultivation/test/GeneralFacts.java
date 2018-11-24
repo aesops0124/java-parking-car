89866ac2a72cddf9b429c19a6fe22a8e276e3ff2
Tommy Hui <aesops0124@gmail.com>
2018-11-24 16:38:11 +0800

    [Tommy] Story 2

--- src/test/java/com/oocl/cultivation/test/GeneralFacts.java
+++ src/test/java/com/oocl/cultivation/test/GeneralFacts.java
@@ -66,4 +66,83 @@ class GeneralFacts {
         assertEquals("Unrecognized parking ticket.", message);
     }
 
+    @Test //s2 ac1
+    void should_not_fetch_any_car_once_ticket_is_not_provided() {
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingLotCenter parkingLotCenter = new ParkingLotCenter(Arrays.asList(parkingLot));
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotCenter);
+        Car car = new Car();
+
+        ParkingTicket ticket = parkingBoy.park(car);
+
+        assertNull(parkingBoy.fetch(null));
+        assertSame(car, parkingBoy.fetch(ticket));
+    }
+
+    @Test //s2 ac2
+    void should_query_message_once_ticket_is_not_provided() {
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingLotCenter parkingLotCenter = new ParkingLotCenter(Arrays.asList(parkingLot));
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotCenter);
+
+        parkingBoy.fetch(null);
+
+        assertEquals(
+            "Please provide your parking ticket.",
+            parkingBoy.getLastErrorMessage());
+    }
+
+    @Test //s2 ac1
+    void should_not_fetch_any_car_once_ticket_has_been_used() {
+        ParkingLotCenter parkingLotCenter = new ParkingLotCenter(Arrays.asList(new ParkingLot()));
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotCenter);
+        Car car = new Car();
+        ParkingTicket ticket = parkingBoy.park(car);
+        parkingBoy.fetch(ticket);
+
+        assertNull(parkingBoy.fetch(ticket));
+    }
+
+    @Test //s2 ac1
+    void should_query_error_message_for_used_ticket() {
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingLotCenter parkingLotCenter = new ParkingLotCenter(Arrays.asList(parkingLot));
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotCenter);
+        Car car = new Car();
+
+        ParkingTicket ticket = parkingBoy.park(car);
+        parkingBoy.fetch(ticket);
+        parkingBoy.fetch(ticket);
+
+        assertEquals(
+            "Unrecognized parking ticket.",
+            parkingBoy.getLastErrorMessage()
+        );
+    }
+
+    @Test //s2 ac3
+    void should_not_park_cars_to_parking_lot_if_there_is_not_enough_position() {
+        final int capacity = 1;
+        ParkingLot parkingLot = new ParkingLot(capacity);
+        ParkingLotCenter parkingLotCenter = new ParkingLotCenter(Arrays.asList(parkingLot));
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotCenter);
+
+        parkingBoy.park(new Car());
+
+        assertNull(parkingBoy.park(new Car()));
+    }
+
+    @Test //s2 ac3
+    void should_get_message_if_there_is_not_enough_position() {
+        final int capacity = 1;
+        ParkingLot parkingLot = new ParkingLot(capacity);
+        ParkingLotCenter parkingLotCenter = new ParkingLotCenter(Arrays.asList(parkingLot));
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLotCenter);
+
+        parkingBoy.park(new Car());
+        parkingBoy.park(new Car());
+
+        assertEquals("The parking lot is full.", parkingBoy.getLastErrorMessage());
+    }
+
 }
